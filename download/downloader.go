69eb05a2ebe60703fd10c21dddb4dfd2fdc8917d
mamad-nik <khoshkhab11@gmail.com>
2023-05-13 10:35:12 +0330

    modularized

---
+++ download/downloader.go
@@ -0,0 +1,98 @@
+package download
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+func Errcheck(err error) {
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+func Getfilenames(theurl string) string {
+	regpat := `/([^/]+)\.html$`
+	re := regexp.MustCompile(regpat)
+	match := re.FindStringSubmatch(theurl)
+	return match[1]
+
+}
+func GetLinks(links *[]string, theurl string) {
+	c := colly.NewCollector()
+
+	c.OnHTML("[data-href]", func(h *colly.HTMLElement) {
+		*links = append(*links, h.Attr("data-href"))
+	})
+
+	c.Visit(theurl)
+
+}
+
+func DownloadFile(link, myfilepath string) {
+	var localSize int64
+	actualSize := Getactualfilesize(link)
+
+	if _, err := os.Stat(myfilepath); !os.IsNotExist(err) {
+		localSize = Getlocalfilesize(myfilepath)
+
+		if actualSize == localSize {
+			fmt.Printf("Skipping %s, file already downloaded\n", filepath.Base(myfilepath))
+			return
+		} else {
+			fmt.Printf("continuing %s, %d MB remaining\n", filepath.Base(myfilepath), (actualSize-localSize)/1048576)
+		}
+
+	} else {
+		fmt.Printf("Downloading %s with size of %d\n", filepath.Base(myfilepath), actualSize/1048576)
+	}
+	since := time.Now()
+	c := http.Client{}
+	req, err := http.NewRequest("GET", link, nil)
+	Errcheck(err)
+
+	s := fmt.Sprintf("bytes 0-%d/%d", localSize, actualSize)
+	req.Header.Add("Content-Range", s)
+
+	r := fmt.Sprintf("bytes=%d-%d", localSize, actualSize-1)
+	req.Header.Add("Range", r)
+
+	err = os.MkdirAll(filepath.Dir(myfilepath), 0755)
+	Errcheck(err)
+	file, err := os.OpenFile(myfilepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	Errcheck(err)
+	resp, err := c.Do(req)
+	Errcheck(err)
+	defer resp.Body.Close()
+	size, err := io.Copy(file, resp.Body)
+	Errcheck(err)
+	t := time.Since(since)
+	fmt.Printf("\n\tDownloaded file: %s with size %d in %v\n\n", filepath.Base(myfilepath), size/1048576, t)
+
+	Errcheck(err)
+}
+
+func Getactualfilesize(url string) int64 {
+	resp, err := http.Head(url)
+	Errcheck(err)
+	defer resp.Body.Close()
+
+	size := resp.ContentLength
+	Errcheck(err)
+	return size
+}
+
+func Getlocalfilesize(filepath string) int64 {
+	info, err := os.Stat(filepath)
+	Errcheck(err)
+	return info.Size()
+}
--- scraper.go
+++ scraper.go
@@ -3,98 +3,11 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os"
-	"path/filepath"
-	"regexp"
 	"strings"
 
-	"github.com/gocolly/colly"
+	"scraper.go/download"
 )
-
-func errcheck(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-func getfilenames(theurl string) string {
-	regpat := `/([^/]+)\.html$`
-	re := regexp.MustCompile(regpat)
-	match := re.FindStringSubmatch(theurl)
-	return match[1]
-
-}
-func getLinks(links *[]string, theurl string) {
-	c := colly.NewCollector()
-
-	c.OnHTML("[data-href]", func(h *colly.HTMLElement) {
-		*links = append(*links, h.Attr("data-href"))
-	})
-
-	c.Visit(theurl)
-
-}
-
-func downloadFile(link, myfilepath string) {
-	var localSize int64
-	actualSize := getactualfilesize(link)
-
-	if _, err := os.Stat(myfilepath); !os.IsNotExist(err) {
-		localSize = getlocalfilesize(myfilepath)
-
-		if actualSize == localSize {
-			fmt.Printf("Skipping %s, file already downloaded\n", filepath.Base(myfilepath))
-			return
-		} else {
-			fmt.Printf("continuing %s, %d MB remaining\n", filepath.Base(myfilepath), (actualSize-localSize)/1048576)
-		}
-
-	} else {
-		fmt.Printf("Downloading %s with size of %d\n", filepath.Base(myfilepath), actualSize/1048576)
-	}
-
-	c := http.Client{}
-	req, err := http.NewRequest("GET", link, nil)
-	errcheck(err)
-
-	s := fmt.Sprintf("bytes 0-%d/%d", localSize, actualSize)
-	req.Header.Add("Content-Range", s)
-
-	r := fmt.Sprintf("bytes=%d-%d", localSize, actualSize-1)
-	req.Header.Add("Range", r)
-
-	err = os.MkdirAll(filepath.Dir(myfilepath), 0755)
-	errcheck(err)
-	file, err := os.OpenFile(myfilepath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	errcheck(err)
-	resp, err := c.Do(req)
-	errcheck(err)
-	defer resp.Body.Close()
-	size, err := io.Copy(file, resp.Body)
-	errcheck(err)
-	fmt.Printf("\n\tDownloaded file: %s with size %d\n\n", filepath.Base(myfilepath), size/1048576)
-
-	errcheck(err)
-}
-
-func getactualfilesize(url string) int64 {
-	resp, err := http.Head(url)
-	errcheck(err)
-	defer resp.Body.Close()
-
-	size := resp.ContentLength
-	errcheck(err)
-	return size
-}
-
-func getlocalfilesize(filepath string) int64 {
-	info, err := os.Stat(filepath)
-	errcheck(err)
-	return info.Size()
-}
 
 // "http://znucomputer.ir/HTML/Semester6/artificial_intelligence.html"
 // "/home/mamad/Downloads"
@@ -105,13 +18,13 @@ func userInput() (string, string) {
 
 	fmt.Print("enter URL of the page you want to download from: ")
 	theurl, err := reader.ReadString('\n')
-	errcheck(err)
+	download.Errcheck(err)
 
 	theurl = strings.TrimSpace(theurl)
 	fmt.Print("Enter the file path to save the downloaded file: ")
 
 	myfilepath, err := reader.ReadString('\n')
-	errcheck(err)
+	download.Errcheck(err)
 	myfilepath = strings.TrimSpace(myfilepath)
 	return theurl, myfilepath
 }
@@ -121,12 +34,12 @@ func main() {
 
 	theurl, myfilepath := "http://znucomputer.ir/HTML/Semester6/artificial_intelligence.html", "/home/mamad/Downloads"
 
-	getLinks(&links, theurl)
+	download.GetLinks(&links, theurl)
 
-	filename := getfilenames(theurl)
+	filename := download.Getfilenames(theurl)
 	for i, v := range links {
 		fmt.Printf("%s: ", v)
 		name := fmt.Sprintf("%s/%s/%s%d", myfilepath, filename, filename, i)
-		downloadFile(v, name)
+		download.DownloadFile(v, name)
 	}
 }
